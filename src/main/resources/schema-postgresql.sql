f21f178243d31661f932e6ff9b67fcc6d5ab761f
Rwata56 <125374222+Rwata56@users.noreply.github.com>
2024-06-09 17:28:02 -0300

    sql e yaml local

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS cliente (
      id serial PRIMARY KEY,
      nome  varchar(50),
-     cpf   varchar(11)
+     email   varchar(50),
+     senha varchar(10),
+     senhaC varchar(10)
 );
